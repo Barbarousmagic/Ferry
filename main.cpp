a156cf566540bfe1eecedb86759c46fcc89e31f4
barbarmgc <novodvorcevmihail@gmail.com>
2026-03-08 20:56:09 +0100

    Initial commit: basic physics loop, hardcoded variables

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main() {
+    double ferryMass = 15000.0; // kg
+    double currentSpeed = 12.5; // m/s
+    double dragCoefficient = 0.04;
+
+    // Physics simulation variables
+    double distanceTraveled = 0.0; // meters
+    double dt = 1.0; // delta t in seconds
+
+
+    while (currentSpeed > 0.1) {
+        // 1. Distance traveled in this time step S = V * t
+        distanceTraveled += currentSpeed * dt;
+
+        // 2. Water drag force F = k * V^2
+        double dragForce = dragCoefficient * (currentSpeed * currentSpeed);
+
+        // 3. Deceleration a = F / m
+        double deceleration = dragForce / ferryMass;
+
+        // 4. Speed update
+        currentSpeed -= deceleration * dt;
+
+        std::cout << "Current speed: " << currentSpeed << "m/s | Distance:" << distanceTraveled<< "m" << std::endl;
+    }
+
+    std::cout << "\nSuccessfully stopped. Braking distance:" << distanceTraveled << std::endl;
+
+    return 0;
+}
