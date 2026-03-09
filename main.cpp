193e698e3a38350cffb5c419f60f71736a933e4c
barbarmgc <novodvorcevmihail@gmail.com>
2026-03-09 03:02:00 +0100

    feat: add water current influence to physics model

--- main.cpp
+++ main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 
-double calcBreakingDist(double mass, double speed, double dragCoeff) {
+double calcBreakingDist(double mass, double speed, double currentWater, double dragCoeff) {
     double distanceTraveled = 0.0;
     double dt = 1.0;
-    while (speed > 0.1) {
+    double relativeSpeed = speed - currentWater;
+    while (relativeSpeed > 0.1) {
         // 1. Distance traveled in this time step S = V * t
         distanceTraveled += speed * dt;
         // 2. Water drag force F = k * V^2
-        double dragForce = dragCoeff * (speed * speed);
+        double dragForce = dragCoeff * (relativeSpeed * relativeSpeed);
         // 3. Deceleration a = F / m
         double deceleration = dragForce / mass;
         // 4. Speed update
         speed -= deceleration * dt;
+        // 5. Relative Speed for next loop step
+        relativeSpeed = speed - currentWater;
     }
     return distanceTraveled;
 }
@@ -19,6 +22,7 @@ double calcBreakingDist(double mass, double speed, double dragCoeff) {
 int main() {
     double ferryMass; // kg
     double currentSpeed; // m/s
+    double currentWater;
     double dragCoefficient = 500.0;
 
     std::cout << "--- IRT Ferry Simulator ---" << std::endl;
@@ -36,7 +40,10 @@ int main() {
     std::cout << "Enter start speed (m/s): ";
     std::cin >> currentSpeed;
 
-    double resultDistance = calcBreakingDist(ferryMass, currentSpeed, dragCoefficient);
+    std::cout << "Enter water speed (m/s): ";
+    std::cin >> currentWater;
+
+    double resultDistance = calcBreakingDist(ferryMass, currentSpeed, currentWater, dragCoefficient);
     std::cout << "\nSuccessfully stopped. Braking distance:" << resultDistance << std::endl;
 
     return 0;
